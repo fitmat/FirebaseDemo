34d9e9298327a91ed88056513548b10b9767d88e
fitmat <fitmatsolutions@gmail.com>
2019-06-24 18:39:38 +0530

    Firebase with Child

--- app/src/main/java/com/fitmat/firebasedemo/Activites.java
+++ app/src/main/java/com/fitmat/firebasedemo/Activites.java
@@ -1,5 +1,8 @@
 package com.fitmat.firebasedemo;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class Activites {
 
 
@@ -8,9 +11,9 @@ public class Activites {
     String minX,minY,maxX,maxY,size,centre,areaCovered,length,width,shape,orientation;
     String actions;
 
+    HashMap<String,String> hashMap = new HashMap<String, String>();
 
-
-    public Activites(String userId, String timestamp, String bufferStream, String minX, String minY, String maxX, String maxY, String size, String centre, String areaCovered, String length, String width, String shape, String orientation, String actions) {
+    public Activites(String userId, String timestamp, String bufferStream, String minX, String minY, String maxX, String maxY, String size, String centre, String areaCovered, String length, String width, String shape, String orientation, String actions, HashMap<String, String> hashMap) {
         this.userId = userId;
         this.timestamp = timestamp;
         this.bufferStream = bufferStream;
@@ -26,127 +29,13 @@ public class Activites {
         this.shape = shape;
         this.orientation = orientation;
         this.actions = actions;
+        this.hashMap = hashMap;
     }
 
-    public String getUserId() {
-        return userId;
-    }
 
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
 
-    public String getTimestamp() {
-        return timestamp;
-    }
 
-    public void setTimestamp(String timestamp) {
-        this.timestamp = timestamp;
-    }
 
-    public String getBufferStream() {
-        return bufferStream;
-    }
-
-    public void setBufferStream(String bufferStream) {
-        this.bufferStream = bufferStream;
-    }
-
-    public String getMinX() {
-        return minX;
-    }
-
-    public void setMinX(String minX) {
-        this.minX = minX;
-    }
-
-    public String getMinY() {
-        return minY;
-    }
-
-    public void setMinY(String minY) {
-        this.minY = minY;
-    }
-
-    public String getMaxX() {
-        return maxX;
-    }
-
-    public void setMaxX(String maxX) {
-        this.maxX = maxX;
-    }
-
-    public String getMaxY() {
-        return maxY;
-    }
-
-    public void setMaxY(String maxY) {
-        this.maxY = maxY;
-    }
-
-    public String getSize() {
-        return size;
-    }
-
-    public void setSize(String size) {
-        this.size = size;
-    }
-
-    public String getCentre() {
-        return centre;
-    }
-
-    public void setCentre(String centre) {
-        this.centre = centre;
-    }
-
-    public String getAreaCovered() {
-        return areaCovered;
-    }
-
-    public void setAreaCovered(String areaCovered) {
-        this.areaCovered = areaCovered;
-    }
-
-    public String getLength() {
-        return length;
-    }
-
-    public void setLength(String length) {
-        this.length = length;
-    }
-
-    public String getWidth() {
-        return width;
-    }
-
-    public void setWidth(String width) {
-        this.width = width;
-    }
-
-    public String getShape() {
-        return shape;
-    }
-
-    public void setShape(String shape) {
-        this.shape = shape;
-    }
-
-    public String getOrientation() {
-        return orientation;
-    }
-
-    public void setOrientation(String orientation) {
-        this.orientation = orientation;
-    }
-
-    public String getActions() {
-        return actions;
-    }
-
-    public void setActions(String actions) {
-        this.actions = actions;
-    }
 }
 
 
--- app/src/main/java/com/fitmat/firebasedemo/MainActivity.java
+++ app/src/main/java/com/fitmat/firebasedemo/MainActivity.java
@@ -12,13 +12,15 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.sql.DatabaseMetaData;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
 //    EditText bufferstream,minX,Actions;
         Button submitData;
 
-    DatabaseReference databaseActivities;
+    DatabaseReference databaseActivities,blobActivities;
 
 
     @Override
@@ -26,7 +28,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        databaseActivities = FirebaseDatabase.getInstance().getReference();
+        databaseActivities = FirebaseDatabase.getInstance().getReference("users");
+       // blobActivities = databaseActivities.getRoot().child("Blob");
 
 //        bufferstream = (EditText)findViewById(R.id.bufferText);
 //        minX = (EditText)findViewById(R.id.BlobProp1);
@@ -47,16 +50,36 @@ public class MainActivity extends AppCompatActivity {
 
     private void addData()
     {
-//        String buffStream = bufferstream.getText().toString().trim();
-//        String BlobProp1=minX.getText().toString().trim();
-//        String Action = Actions.getText().toString().trim();
-
         long millis = System.currentTimeMillis();
         String Time = String.valueOf(millis);
 
         String id = databaseActivities.push().getKey();
+
+        String minX = "1500";
+        String maxX = "2000";
+        String minY = "1500";
+        String maxY = "2000";
+        String centre = "1500";
+        String length = "2000";
+        String width = "1500";
+
+        HashMap<String,String> hm= new HashMap<>();
+        hm.put("Min X",minX);
+        hm.put("Max X",maxX);
+        hm.put("Min Y",minY);
+        hm.put("Max Y",maxY);
+        hm.put("centre",centre);
+        hm.put("length",length);
+        hm.put("Width",width);
+        hm.put("Min X",minX);
+        hm.put("Max X",maxX);
+        hm.put("Min Y",minY);
+        hm.put("Max Y",maxY);
+        hm.put("centre",centre);
+        hm.put("length",length);
+        hm.put("Width",width);
         Activites activites = new Activites(id,Time,"AAB1205465456","10","12","36",
-                "40","12","56","32","100","14","Triangle","Vertical","Jump");
+                "40","12","56","32","100","14","Triangle","Vertical","Jump",hm);
 
         databaseActivities.child(id).setValue(activites);
 
